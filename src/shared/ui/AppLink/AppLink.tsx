660c21fadaca39d3d79e3439f5f528abd9e71475
Irina Kirillova <keira.kirillova@gmail.com>
2023-08-21 20:40:04 +0200

    update AppLink

--- src/shared/ui/AppLink/AppLink.tsx
+++ src/shared/ui/AppLink/AppLink.tsx
@@ -1,7 +1,11 @@
 import { NavLink, NavLinkProps } from 'react-router-dom';
 import classes from './AppLink.module.scss';
 
-export const AppLink: React.FC<NavLinkProps> = ({ to, children, ...otherProps }) => {
+interface AppLinkProps extends NavLinkProps {
+    to: string;
+}
+
+export const AppLink: React.FC<AppLinkProps> = ({ to, children, ...otherProps }) => {
     return (
         <NavLink
             to={to}
